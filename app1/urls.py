a6e2a2e2c483f7b3b5cfb9d683f05dff80f99c99
vohongphuc2003 <vhp17203@gmail.com>
2023-11-23 14:12:57 +0700

    tạo trang chu 1

--- app1/urls.py
+++ app1/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views 
+
+
 urlpatterns = [
-    path('',views.index),
-    path('/1', views.index1)
+    path('',views.trangchu, name='Home'),
+    path('/1', views.base, name='base')
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request) :
-    return HttpResponse("This is app 1")
-def index1(request) :
-    return HttpResponse("This is app's child 1")
+def trangchu(request) :
+    return render(request , 'web1/trangchu.html')
+def base(request) :
+    return render(request , 'web1/base.html')
